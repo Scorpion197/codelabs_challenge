9865a4527ba8a8bb1261257a269fa091b19ae627
Scorpion197 <doudou.gaw@gmail.com>
2022-01-28 00:38:55 +0100

    updated surveys.json

--- server/index.js
+++ server/index.js
@@ -12,6 +12,7 @@ app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
 app.use(cors());
 app.use((req, res ,next) => {
 
+    //security issue, fix later
     res.header("Access-Control-Allow-Origin", "*")
     next()
 })
