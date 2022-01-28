99f580941aba89c0a7c38d8483d0cd3da3226aec
Scorpion197 <doudou.gaw@gmail.com>
2022-01-28 18:11:49 +0100

    finished styling pagination

--- frontend/src/components/pagination/Pagination.js
+++ frontend/src/components/pagination/Pagination.js
@@ -9,16 +9,16 @@ const Pagination = ({questionsPerPage, allQuestions, paginate}) => {
     return (
 
         <nav>
-            <ul>
+            <div class="flex flex-row ml-[-4%] mt-[5px] mb-[10px]">
                 {pageNumbers.map((number) => (
 
-                    <li key={number}>
-                        <a class="cursor-pointer"onClick={() => paginate(number)}>
+                    <div key={number} class=" hover:bg-[#3498db] hover:text-[#fff] rounded-[4px] cursor-pointer flex flex-row w-[5vh] border-[1px] ml-[0.5px] border-[#bdc3c7] text-[#3498db] ">
+                        <a class="flex flex-col w-[80%] m-auto items-center justify-center" onClick={() => paginate(number)}>
                             {number}
                         </a>
-                    </li>
+                    </div>
                 ))}
-            </ul>
+            </div>
         </nav>
     )
 }
